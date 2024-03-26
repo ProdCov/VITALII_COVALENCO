c8134f76131cb694973d82fc6155d1866df0c4ed
Iulia Miloserdova <iulia.miloserdova@isa.utm.md>
2024-03-27 00:32:43 +0200

    Important messages in Java.

---
+++ src/main/java/homework_nr_2/HomeWork2.java
@@ -0,0 +1,31 @@
+package homework_nr_2;
+
+public class HomeWork2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        long currentWorldPopulation = 7811080815L;
+        int carsProducedThisYear = 55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaIsAmazing = true;
+        float pi = 3.14159f;
+        System.out.println("----------------");
+        System.out.println(currentWorldPopulation);
+        System.out.println(carsProducedThisYear);
+        System.out.println(firstLetterInAlphabet);
+        System.out.println(javaIsAmazing);
+        System.out.println(pi);
+        System.out.println("-----------");
+        double price = 2.50;
+        double tax = 0.50;
+        int quantity = 1327;
+        double sum = 0;
+        System.out.println(price);
+        System.out.println(tax);
+        System.out.println(quantity);
+        System.out.println(sum);
+        System.out.println("-------------");
+        double total = price + tax * quantity + sum;
+        System.out.println(total);
+    }
+}
